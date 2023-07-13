20ca7485c1e65048d0645b002bfce840612d854e
Wassim Wazzi <wassim.wazzi@mail.mcgill.ca>
2023-07-13 19:19:01 -0400

    add base communicator

---
+++ utils/communicators/base_communicator.js
@@ -0,0 +1,66 @@
+/**
+ * absract Base communicator class.
+ * Used to communicate with 3rd party music platforms.
+ *
+ * @class BaseCommunicator
+ * @abstract
+ */
+class BaseCommunicator {
+  constructor() {
+    if (this.constructor === BaseCommunicator) {
+      throw new TypeError('Abstract class "BaseCommunicator" cannot be instantiated directly.')
+    }
+  }
+
+  /**
+   * Gets all the playlists of the user.
+   *
+   * @param {Object} _req - The request object.
+   * @param {Object} _res - The response object.
+   * @returns {Object} The playlists of the user.
+   * @abstract
+   * @memberof BaseCommunicator
+   * @instance
+   * @function getPlaylists
+   * @throws {TypeError} - If the method is not implemented.
+   */
+  getPlaylists(_req, _res) {
+    throw new TypeError('getPlaylists method must be implemented.')
+  }
+
+  /**
+   * Gets all the tracks of a playlist.
+   *
+   * @param {Object} _req - The request object.
+   * @param {Object} _res - The response object.
+   * @param {string} _playlistId - The id of the playlist.
+   * @returns {Object} The tracks of the playlist.
+   * @abstract
+   * @memberof BaseCommunicator
+   * @instance
+   * @function getTracks
+   * @throws {TypeError} - If the method is not implemented.
+   */
+  getTracks(_req, _res, _playlistId) {
+    throw new TypeError('getTracks method must be implemented.')
+  }
+
+  /**
+   * Add tracks to a playlist.
+   *
+   * @param {Object} _req - The request object.
+   * @param {Object} _res - The response object.
+   * @param {string} _playlistId - The id of the playlist.
+   * @param {Array} _tracks - The tracks to add to the playlist.
+   * @abstract
+   * @memberof BaseCommunicator
+   * @instance
+   * @function addTracks
+   * @throws {TypeError} - If the method is not implemented.
+   */
+  addTracks(_req, _res, _playlistId, _tracks) {
+    throw new TypeError('addTracks method must be implemented.')
+  }
+}
+
+export default BaseCommunicator
