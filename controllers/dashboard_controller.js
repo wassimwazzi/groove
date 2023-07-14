7d5231553b201d8ad37e2aa2dd8a24a9dc8eefb8
Wassim Wazzi <wassim.wazzi@mail.mcgill.ca>
2023-07-14 10:04:59 -0400

    dashboard gets playlists

--- controllers/dashboard_controller.js
+++ controllers/dashboard_controller.js
@@ -1,17 +1,16 @@
 import titelize from '../utils/titelize.js'
 import PlatformsManager from '../utils/platforms_manager.js'
-// import SpotifyCommunicator from '../utils/communicators/spotify_communicator.js'
 
 export default async (req, res) => {
   const platform = req.query.platform
   const authenticator = PlatformsManager.getAuthenticator(platform)
   if (!authenticator.isLoggedIn(req)) {
-    req.flash('alert', 'Connect to one of the platforms first')
+    req.flash('alert', 'Connect to one of the platforms to access the dashboard')
     res.redirect('/')
     return
   }
-  // const communicator = new SpotifyCommunicator()
-  // const playlists = await communicator.getPlaylists(req)
-  // console.log(playlists)
-  res.render('dashboard', { platform: titelize(platform) })
+  const communicator = PlatformsManager.getCommunicator(platform)
+  const playlists = await communicator.getPlaylists(req)
+  console.log(playlists)
+  res.render('dashboard', { platform: titelize(platform), playlists: playlists })
 }
--- test/controllers/dashboard_controller.spec.js
+++ test/controllers/dashboard_controller.spec.js
@@ -6,15 +6,31 @@ describe('authController', () => {
   let req
   let res
   let isLoggedInStub
+  let getPlaylistsStub
+  let getAuthenticatorStub
+  let getCommunicatorStub
 
   beforeEach(() => {
-    req = { query: { platform: 'spotify' }, flash: sinon.stub() }
+    req = {
+      query: { platform: 'spotify' },
+      flash: sinon.stub(),
+      session: {
+        spotifyAccessToken: 'spotifyAccessToken',
+      },
+    }
     res = {
       redirect: sinon.stub(),
       render: sinon.stub(),
     }
 
     isLoggedInStub = sinon.stub()
+    getPlaylistsStub = sinon.stub()
+    getAuthenticatorStub = sinon.stub(PlatformsManager, 'getAuthenticator').returns({
+      isLoggedIn: isLoggedInStub,
+    })
+    getCommunicatorStub = sinon.stub(PlatformsManager, 'getCommunicator').returns({
+      getPlaylists: getPlaylistsStub,
+    })
   })
 
   afterEach(() => {
@@ -22,10 +38,6 @@ describe('authController', () => {
   })
 
   it('should redirect if the user is not logged in', async () => {
-    const getAuthenticatorStub = sinon.stub(PlatformsManager, 'getAuthenticator').returns({
-      isLoggedIn: isLoggedInStub,
-    })
-
     isLoggedInStub.returns(false)
 
     await dashboardController(req, res)
@@ -38,18 +50,35 @@ describe('authController', () => {
   })
 
   it('should render the dashboard if the user is logged in', async () => {
-    const getAuthenticatorStub = sinon.stub(PlatformsManager, 'getAuthenticator').returns({
-      isLoggedIn: isLoggedInStub,
-    })
-
     isLoggedInStub.returns(true)
 
     await dashboardController(req, res)
 
     sinon.assert.calledOnce(getAuthenticatorStub)
     sinon.assert.calledOnce(res.render)
-    sinon.assert.calledWith(res.render, 'dashboard', { platform: 'Spotify' })
+    sinon.assert.calledWithMatch(res.render, 'dashboard', { platform: 'Spotify' })
     sinon.assert.notCalled(res.redirect)
     sinon.assert.notCalled(req.flash)
+  })
+
+  it('should get the playlists if the user is logged in', async () => {
+    isLoggedInStub.returns(true)
+    getPlaylistsStub.returns([])
+
+    await dashboardController(req, res)
+
+    sinon.assert.calledOnce(getCommunicatorStub)
+    sinon.assert.calledOnce(res.render)
+    sinon.assert.calledWith(res.render, 'dashboard', { platform: 'Spotify', playlists: [] })
+  })
+
+  it('should not get the playlists if the user is not logged in', async () => {
+    isLoggedInStub.returns(false)
+
+    await dashboardController(req, res)
+
+    sinon.assert.notCalled(getCommunicatorStub)
+    sinon.assert.calledOnce(res.redirect)
+    sinon.assert.calledWith(res.redirect, '/')
   })
 })
