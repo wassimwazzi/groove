4dcc71c38554e9fe2f67201dfed893f01d60cee9
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2023-07-20 10:25:25 -0400

    use https

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import flashMiddleware from './middleware/flash_middleware.js'
 import flash from 'connect-flash'
 import { port, host, HTTP_SECURE } from './config.js'
 import crypto from 'crypto'
+import https from 'https'
+import fs from 'fs'
 
 export const app = express()
 
@@ -30,6 +32,18 @@ app
   .use('/images', express.static('images'))
   .use(routes)
 
-const listener = app.listen(port, host, () => {
-  console.log(`Server is listening on ${listener.address().address}:${listener.address().port}`)
-})
+HTTP_SECURE
+  ? https
+      .createServer(
+        {
+          key: fs.readFileSync('certs/key.pem'),
+          cert: fs.readFileSync('certs/cert.pem'),
+        },
+        app,
+      )
+      .listen(port, host, () => {
+        console.log(`Server is listening on ${host}:${port}`)
+      })
+  : app.listen(port, host, () => {
+      console.log(`Server is listening on ${host}:${port}`)
+    })
