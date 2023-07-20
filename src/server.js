d47d85c265cfb5d8881414ac0daaf5d54c7aa01d
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2023-07-20 10:38:15 -0400

    remove https as heroku will handle it on prod

--- src/server.js
+++ src/server.js
@@ -7,8 +7,6 @@ import flashMiddleware from './middleware/flash_middleware.js'
 import flash from 'connect-flash'
 import { port, host, HTTP_SECURE } from './config.js'
 import crypto from 'crypto'
-import https from 'https'
-import fs from 'fs'
 
 export const app = express()
 
@@ -32,18 +30,6 @@ app
   .use('/images', express.static('images'))
   .use(routes)
 
-HTTP_SECURE
-  ? https
-      .createServer(
-        {
-          key: fs.readFileSync('certs/key.pem'),
-          cert: fs.readFileSync('certs/cert.pem'),
-        },
-        app,
-      )
-      .listen(port, host, () => {
-        console.log(`Server is listening on ${host}:${port}`)
-      })
-  : app.listen(port, host, () => {
-      console.log(`Server is listening on ${host}:${port}`)
-    })
+app.listen(port, host, () => {
+  console.log(`Server is listening on ${host}:${port}`)
+})
