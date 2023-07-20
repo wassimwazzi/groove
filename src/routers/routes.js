ded5becba122d137a6b6525c75a49c18ae07f6d0
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2023-07-20 10:02:11 -0400

    use one routes file

---
+++ src/routers/routes.js
@@ -0,0 +1,6 @@
+import authRouter from './auth_router.js'
+import dashboardRouter from './dashboard_router.js'
+import playlistsRouter from './playlists_router.js'
+import playerRouter from './player_router.js'
+
+export default [authRouter, dashboardRouter, playlistsRouter, playerRouter]
