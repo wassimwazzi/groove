2a10ce1d345b084fd1669e645ce37a2ae8d12302
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2023-07-20 23:49:10 -0400

    fix pipeline test

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 function valueOrError(key) {
   const value = process.env[key]
-  if (!value) {
+  if (!value && process.env.NODE_ENV !== 'test') {
     throw new Error(`Missing environment variable ${key}`)
   }
   return value
